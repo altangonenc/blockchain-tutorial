bc62d00074dbada138408ac07368ff983787b211
altangonenc <altangonenc10@gmail.com>
2023-10-17 12:50:15 +0300

    Initial commit

---
+++ example/src/main/java/Block.java
@@ -0,0 +1,41 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+public class Block {
+    private String hash;
+    private String previousHash;
+    private String data;
+    private long timeStamp;
+    private int nonce;
+
+    public Block(String data, String previousHash, long timeStamp) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = timeStamp;
+        this.hash = calculateBlockHash();
+    }
+
+    public String calculateBlockHash(){
+        String dataToHash = previousHash
+                + Long.toString(timeStamp)
+                + Integer.toString(nonce)
+                + data;
+        MessageDigest digest = null;
+        byte[] bytes = null;
+
+        try{
+            digest = MessageDigest.getInstance("SHA-256");
+            bytes = digest.digest(dataToHash.getBytes(StandardCharsets.UTF_8));
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+
+        StringBuffer buffer = new StringBuffer();
+
+        for (byte b : bytes) {
+            buffer.append(String.format("%02x", b));
+        }
+
+        return buffer.toString();
+    }
+}
